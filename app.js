64e075a98e246d51c3fc64d68cab0e902a4c1e71
Avynn Donaghe <avynndonaghe@Avynns-MacBook-Pro.local>
2017-01-17 19:48:52 -0800

    First working version

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var nodemon = require('nodemon');
 var store = require('json-fs-store')();
+var router = express.Router();
 app.use(bodyParser.json());
 
-function team(scrMar, threePntPrct, rebMar, trnOvrMar, id, name) {
-	this.scrMar = scrMar;
-	this.threePntPrct = threePntPrct;
+function team(FreThrowMade, feilGolPrct, rebMar, trnOvrMar, id, name) {
+	this.FreThrowMade = FreThrowMade;
+	this.feilGolPrct = feilGolPrct;
 	this.rebMar = rebMar;
 	this.trnOvrMar = trnOvrMar;
 	this.id = id;
@@ -15,21 +17,212 @@ function team(scrMar, threePntPrct, rebMar, trnOvrMar, id, name) {
 };
 
 var idNum = ''
-
 var teamDataResponse = ''
+var FreThrowMadeRankings = [];
+var feilGolPrctRankings = [];
+var rebMarRankings = [];
+var trnOvrMarRankings = [];
+var storeList = [];
+var FreThrowMadeWeight = .15
+var feilGolPrctWeight = .4
+var rebMarWeight= .2
+var trnOvrMarWeight = .15
+
+
+populateRankingsId = ''
+
+function populateRankings(callback){
+	store.list(function(err, objects){
+		storeList = objects;
+		storeList.splice(0,1)
+		//console.log(JSON.stringify(objects));
+		FreThrowMadeRankings = objects.map(function(obj){
+			var rObj = {};
+			rObj = {
+				team: obj.name,
+				value: obj.FreThrowMade
+			}
+			return rObj;
+		});
+		FreThrowMadeRankings.splice(0,1);
+		//console.log(FreThrowMadeRankings);
+		feilGolPrctRankings = objects.map(function(obj){
+			var rObj = {};
+			rObj = {
+				team: obj.name,
+				value: obj.feilGolPrct
+			}
+			return rObj;
+		});
+		feilGolPrctRankings.splice(0,1);
+		//console.log(feilGolPrctRankings);
+		rebMarRankings = objects.map(function(obj){
+			var rObj = {};
+			rObj = {
+				team: obj.name,
+				value: obj.rebMar
+			}
+			return rObj;
+		});
+		rebMarRankings.splice(0,1);
+		//console.log(rebMarRankings);
+		trnOvrMarRankings = objects.map(function(obj){
+			var rObj = {};
+			rObj = {
+				team: obj.name,
+				value: obj.rebMar
+			}
+			return rObj;
+		});
+		trnOvrMarRankings.splice(0,1);
+		//console.log(trnOvrMarRankings);
+		callback();
+	});
+};
+
+function sortRankings(callback){
+	console.log("===========");
+	FreThrowMadeRankings.sort(function (a, b) {
+		/*console.log(a);
+		console.log("============")
+		console.log(b);*/
+  		if (a.value > b.value) {
+  			//console.log("a is greater than b");
+    		return 1;
+  		}
+  		if (a.value < b.value) {
+  			//console.log("b is greater than a");
+    		return -1;
+ 		}
+  		// a must be equal to b
+  		console.log("a and b are equal");
+  		return 0;
+	});
+	console.log("FreThrowMadeRankings")
+	console.log(FreThrowMadeRankings);
+	feilGolPrctRankings.sort(function (a, b) {
+		/*console.log(a);
+		console.log("============")
+		console.log(b);*/
+  		if (a.value > b.value) {
+  			//console.log("a is greater than b");
+    		return 1;
+  		}
+  		if (a.value < b.value) {
+  			///console.log("b is greater than a");
+    		return -1;
+ 		}
+  		// a must be equal to b
+  		console.log("a and b are equal");
+  		return 0;
+	});
+	console.log("feilGolPrctRankings");
+	console.log(feilGolPrctRankings)
+	rebMarRankings.sort(function (a, b) {
+		/*console.log(a);
+		console.log("============")
+		console.log(b);*/
+  		if (a.value > b.value) {
+  			//console.log("a is greater than b");
+    		return 1;
+  		}
+  		if (a.value < b.value) {
+  			//console.log("b is greater than a");
+    		return -1;
+ 		}
+  		// a must be equal to b
+  		console.log("a and b are equal");
+  		return 0;
+	});
+	console.log("rebMarRankings")
+	console.log(rebMarRankings);
+	trnOvrMarRankings.sort(function (a, b) {
+		/*console.log(a);
+		console.log("============")
+		console.log(b);*/
+  		if (a.value > b.value) {
+  			//console.log("a is greater than b");
+    		return 1;
+  		}
+  		if (a.value < b.value) {
+  			//console.log("b is greater than a");
+    		return -1;
+ 		}
+  		// a must be equal to b
+  		console.log("a and b are equal");
+  		return 0;
+	});
+	console.log("trnOvrMarRankings");
+	console.log(trnOvrMarRankings);
+	console.log("=========")
+	callback();
+};
+
+var calculateScore =  function(obj){
+	var FreThrowMadeRankingsWorst = FreThrowMadeRankings[0].value;
+	var FreThrowMadeRankingsBest = FreThrowMadeRankings[FreThrowMadeRankings.length - 1].value;
+	var feilGolPrctRankingsWorst = feilGolPrctRankings[0].value;
+	var feilGolPrctRankingsBest = feilGolPrctRankings[feilGolPrctRankings.length - 1].value;
+	var rebMarRankingsWorst = rebMarRankings[0].value;
+	var rebMarRankingsBest = rebMarRankings[rebMarRankings.length - 1].value;
+	var trnOvrMarRankingsWorst = trnOvrMarRankings[0].value;
+	var trnOvrMarRankingsBest = trnOvrMarRankings[trnOvrMarRankings.length - 1].value;
+	obj.forEach(function(element){
+		FreThrowMadeOutputRaw = (element.FreThrowMade - FreThrowMadeRankingsWorst)/(FreThrowMadeRankingsBest - FreThrowMadeRankingsWorst);
+		FreThrowMadeOutput = FreThrowMadeOutputRaw * FreThrowMadeWeight;
+		//element.FreThrowMadeOutput = FreThrowMadeOutput;
+		//console.log(FreThrowMadeOutput);
+		feilGolPrctOutputRaw = (element.feilGolPrct - feilGolPrctRankingsWorst)/(feilGolPrctRankingsBest - feilGolPrctRankingsWorst);
+		feilGolPrctOutput = feilGolPrctOutputRaw * feilGolPrctWeight;
+		//element.feilGolPrctOutput = feilGolPrctOutput;
+		//console.log(feilGolPrctOutput);
+		rebMarOutputRaw = (element.rebMar - rebMarRankingsWorst)/(rebMarRankingsBest - rebMarRankingsWorst);
+		rebMarOutput = rebMarOutputRaw * rebMarWeight;
+		//element.rebMarOutput = rebMarOutput;
+		//console.log(rebMarOutput);
+		trnOvrMarOutputRaw = (element.trnOvrMar - trnOvrMarRankingsWorst)/(trnOvrMarRankingsBest - trnOvrMarRankingsWorst);
+		trnOvrMarOutput = trnOvrMarOutputRaw * trnOvrMarWeight;
+		//element.trnOvrMarOutput = trnOvrMarOutput;
+		element.totalOutput = FreThrowMadeOutput + feilGolPrctOutput + rebMarOutput + trnOvrMarOutput;
+	});
+	//console.log(obj);
+	storeList = obj;
+	storeList.sort(function (a,b){
+		if (a.totalOutput > b.totalOutput) {
+  			//console.log("a is greater than b");
+    		return -1;
+  		}
+  		if (a.totalOutput < b.totalOutput) {
+  			//console.log("b is greater than a");
+    		return 1;
+ 		}
+  		// a must be equal to b
+  		console.log("a and b are equal");
+  		return 0;
+	})
+	console.log(storeList);
+};
+
 
 store.load('1', function(err, object){
 	idNum = object;
+	var idNumNum = object.data;
+	console.log("current id status: " + JSON.stringify(object));
+	var populateRankingsId = idNumNum + 1
+	populateRankings(function(){
+		sortRankings(function(){
+			//console.log(storeList);
+			calculateScore(storeList);
+		});
+	});
+	//calculateScore();
 	if(err) throw err;
-})
+});
 
 app.get('/teamdata', function(req,res){
 	console.log("incoming request for teamdata");
 	res.type('json');
-	store.list(function(err, objects){
-		if(err) throw err;
-		res.end(JSON.stringify(objects));
-	});
+	res.end(JSON.stringify(storeList));
 });
 
 app.get('/ajax', function(req, res){
@@ -40,16 +233,15 @@ app.get('/ajax', function(req, res){
 });
 
 app.post('/log', function(req, res){
-	console.log(req.body.scrMar + " " + req.body.threePntPrct + " " + req.body.rebMar + " " + req.body.trnOvrMar + " "+ req.body.name + " " + req.body.id);
+	console.log(req.body.FreThrowMade + " " + req.body.feilGolPrct + " " + req.body.rebMar + " " + req.body.trnOvrMar + " "+ req.body.name + " " + req.body.id);
 	res.json(200, {status: "success"});
-	var reqName = req.body.name
-	reqName = new team(req.body.scrMar, req.body.threePntPrct, req.body.rebMar, req.body.trnOvrMar, req.body.id, req.body.name);
-	console.log(reqName);
+	var teamData = new team(req.body.FreThrowMade, req.body.feilGolPrct, req.body.rebMar, req.body.trnOvrMar, req.body.id, req.body.name);
+	console.log(teamData.name);
 	var newId = {
 		id: 1,
 		data: req.body.id
 	}
-	store.add(reqName, function(err){
+	store.add(teamData, function(err){
 		if(err) throw err;
 	});
 	store.add(newId, function(err){
@@ -58,8 +250,10 @@ app.post('/log', function(req, res){
 
 });
 
+
+app.use('/api', router);
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(8080, function(){	
+var server = app.listen(8080, function(){
 	console.log('server running on 8080');
 });
--- public/script.js
+++ public/script.js
@@ -10,6 +10,22 @@ $(function(){
 			id = response.data
 		}
 	});
+	$.ajax({
+		type: "GET",
+		url: "http://localhost:8080/teamdata",
+		success: function(response){
+			console.log("connected2");
+			console.log(JSON.stringify(response));
+			for (var i = 0; i < id + 1; i++){
+				console.log(i);
+				console.log('==');
+				console.log(id);
+				var appendedTeam = response[i];
+				console.log(JSON.stringify(appendedTeam["name"]))
+				$('.table').append('<tr>'+ '<td>' + (i + 1) + '</td>' + '<td>' + JSON.stringify(appendedTeam["name"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["FreThrowMade"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["feilGolPrct"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["rebMar"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["trnOvrMar"]) + '</td>' + '</tr>');
+			}
+		}
+	});
 });
 
 $('.load').click(function(){
@@ -20,17 +36,21 @@ $('.load').click(function(){
 		success: function(response){
 			console.log("connected2");
 			console.log(JSON.stringify(response));
-			for(var key in response){
-				console.log(response.keys(response)[key]);
-				
+			for (var i = 1; i < id + 1; i++){
+				console.log(i);
+				console.log('==');
+				console.log(id);
+				var appendedTeam = response[i];
+				console.log(JSON.stringify(appendedTeam["name"]))
+				$('.table').append('<tr>'+ '<td>' + JSON.stringify(appendedTeam["name"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["FreThrowMade"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["feilGolPrct"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["rebMar"]) + '</td>' + '<td>' + JSON.stringify(appendedTeam["trnOvrMar"]) + '</td>' + '</tr>');
 			}
 		}
 	});	
 });
 
 
-var usrScrMar = document.getElementById('scrMar');
-var usrThreePntPrct = document.getElementById('threePntPrct');
+var usrFreThrowMade = document.getElementById('FreThrowMade');
+var usrfeilGolPrct = document.getElementById('feilGolPrct');
 var usrRebMar = document.getElementById('rebMar');
 var usrTrnOvrMar = document.getElementById('trnOvrMar');
 var usrName = document.getElementById('name');
@@ -40,24 +60,25 @@ var usrName = document.getElementById('name');
 $('.btn').click(function(){
 	id = parseInt(id) + 1;
 	console.log(id);
+	console.log(usrFreThrowMade.value);
 	$.ajax({
 		type: "POST",
 		url:"log",
 		contentType: "application/json",
 		data: JSON.stringify({
-			scrMar: usrScrMar.value,
-			threePntPrct: usrThreePntPrct.value,
+			FreThrowMade: usrFreThrowMade.value,
+			feilGolPrct: usrfeilGolPrct.value,
 			rebMar: usrRebMar.value,
 			trnOvrMar: usrTrnOvrMar.value,
 			name: usrName.value,
 			id: id
 		})
-	})
+	});
 	console.log(JSON.stringify(id));
 	console.log("button works!");
 	console.log(usrName.value);
-	console.log(usrScrMar.value);
-	console.log(usrRank.value);
+	console.log(usrFreThrowMade.value);
+	console.log(usrfeilGolPrct.value);
 	console.log(usrRebMar.value);
 	console.log(usrTrnOvrMar.value);
 });
